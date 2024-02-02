2c47cb4dead602cf9d7d5c7ddaaaf76f961e21e1
Markost1 <markostevovic565@gmail.com>
2024-02-02 10:38:26 +0100

    dodavanje redux persista kako bi cuvali korisnicke podatke u localStorage i ima li im pristup nakon refresovanja stranice

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import  userReducer  from './user/userSlice' //uvezli smo userSlice.reducer
+import {persistReducer, persistStore} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+
+//kad instaliramo redux persist idemo u store.
+const rootReducer = combineReducers({
+  user: userReducer
+})
+
+const persistConfig = {
+  key:'root',
+  storage,
+  version:1
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 
 export const store = configureStore({
-  reducer: {user:userReducer},
+  reducer: persistedReducer,//{user:userReducer}, umjesto ovoga satvio sam persistedReducer
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({ //ovaj dio je znacajan da izbjegnemo greske kasnije
     serializableCheck: false, //ovaj dio
   })
@@ -11,7 +27,4 @@ export const store = configureStore({
   }
 )
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
-//export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-//export type AppDispatch = typeof store.dispatch
+export const persistor = persistStore(store)
