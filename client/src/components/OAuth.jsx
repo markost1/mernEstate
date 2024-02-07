ae42d4bc7f5c2f19f8317b85e6d5e1118829b405
Markost1 <markostevovic565@gmail.com>
2024-02-07 11:10:33 +0100

    Zavrsena frontend sekcija OAuth

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,14 +1,31 @@
 import  { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
 import { app } from '../firebase';
+import { useDispatch } from "react-redux";
+import {  signInSuccess } from '../redux/user/userSlice'
 
 export default function OAuth() {
+  const dispatch = useDispatch();
   const handleGoogleClick = async() => {
     try {
       const provider = new GoogleAuthProvider();
       const auth = getAuth(app)
 
       const result =  await signInWithPopup(auth,provider)
-      console.log(result);
+      //console.log(result);
+      //3:12:50
+      const res = await fetch('/api/auth/google',{
+        method:"POST",
+        headers:{
+          'Content-Type':'application/json',
+        },
+        body: JSON.stringify({
+          name:result.user.displayName,
+          email:result.user.email,
+          photo:result.user.photoURL
+        }),
+      })
+      const data = await res.json()
+      dispatch(signInSuccess(data))
     } catch (error) {
       console.log('could not sign in with google',error);
     }
