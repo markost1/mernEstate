eff6f63d33a959821e7ab24bdb938bf78bb47257
Markost1 <markostevovic565@gmail.com>
2024-02-11 00:08:42 +0100

    Use Ref Hook

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,14 +1,18 @@
 // import React from 'react'
+import { useRef } from "react";
 import { useSelector } from "react-redux";
+
 
 export default function Profile() {
   const {currentUser} = useSelector((state) => state.user )
+  const fileRef = useRef(null)
   return (
     <div className="max-w-lg mx-auto p-3">
       <h1 className="text-center my-7 text-3xl font-semibold">Profile</h1>
 
       <form className="flex flex-col gap-4">
-        <img className="rounded-full h-24 w-24 self-center object-cover cursor-pointer mt-2" src={currentUser.avatar} alt='profile'/>
+        <input type="file" ref={fileRef} hidden accept="image/*"/>
+        <img onClick={()=>fileRef.current.click()} className="rounded-full h-24 w-24 self-center object-cover cursor-pointer mt-2" src={currentUser.avatar} alt='profile'/>
         <input type ='text' className="border rounded-xl p-3" placeholder="username" id='username'/>
         <input type ='email' className="border rounded-xl p-3" placeholder="email" id='email'/>
         <input type ='password' className="border rounded-xl p-3" placeholder="password" id='password'/>
