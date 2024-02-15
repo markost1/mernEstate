3ddd305d9b5a968087c54022d2bb3de6d11cbf38
Markost1 <markostevovic565@gmail.com>
2024-02-15 21:41:34 +0100

    Kreirana user update api ruta 2 dio

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,7 @@
+import { errorHandler } from "../utils/error.js"
+import bcrypt  from 'bcryptjs'
+import User from '../models/user.model.js'
+
 export const test = (req,res)=>{
     res.json({
         message:"Hello WORLD"
@@ -7,6 +11,57 @@ export const test = (req,res)=>{
 //koristimo token koji je kreiran u cookie , taj token je kreiran prilikom sign in korisnika
 //koristimo taj token da bi vertifikovali korisnika
 // da bi provjerili token pravimo funkciju verifyUser u utills 
-export const updateUser = (req,res,next) =>{
+
+export const updateUser = async (req,res,next) =>{
+
+    // u verifyToken dobijamo req.user korisnika kome je ispravan token
+    //prvo uporedjuemo id tog korisnika sa id em iz rute :id
+
+    if(req.user.id !== req.params.id) return next(errorHandler('401','You are not autentificated'))
+
+    //ako se podudara id koristimo try catch
+    try {
+      //ako korisnik zeli da promjeni svoj password moramo da hashujemo password
+      if(req.body.password){
+        req.body.password = bcrypt.hashSync(req.body.password,10)
+
+        //updejtovacemo kornisnika imamo User model koristimo mongoose funkciju findByIdAndUpdate
+        //prvo odredjujemo koji cemo id da updejtujemo a onda pastujemo podatke 
+        const updateUser = await User.findByIdAndUpdate(req.params.id,{
+            $set:{
+                username: req.body.username,
+                email:req.body.email,
+                password: req.body.password,
+                avatar: req.body.avatar,
+            },
+        },{new:true}   // updejtuje bazu sa podacima koji predstavljaju nove informacije.
+        ) 
+
+        //nakon ovoga moramo da odvojimo password od ostalog
+        const {password, ...rest} = updateUser._doc
+
+        //saljemo odgovor 
+        res.status(200).json(rest);
+        //prelazim na testiranje api rute sa insomnijom
+      }  
+    } catch (error) {
+        next(error)
+    }
 
 }
+
+// ovo je odgovor servera podaci bez password koji predstavlja povjerljivu informaciju
+/* {
+	"avatar": "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+	"_id": "65b37cf0a74839ed6d95dff1",
+	"username": "test2",
+	"email": "test2@gmail.com",
+	"createdAt": "2024-01-26T09:35:44.263Z",
+	"updatedAt": "2024-01-26T09:35:44.263Z",
+	"__v": 0
+}*/
+
+//uzimam id i idem u insomiju da updejtujem podatke korisnika preko id-a
+
+
+//ne radi dobro ne mogu da mjenjam podatke
--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -2,21 +2,26 @@
 //ovaj paket mora de inicijalizuje u index.js
 
 import jwt from "jsonwebtoken";
-import { errorHandler } from "./error"
+import { errorHandler } from "./error.js"
 
 export const verifyToken = (req,res,next) =>{
-    const token = req.cookie.access_token //uzimamo podatke iz cookie vezane za token
+    const token = req.cookies.access_token //uzimamo podatke iz cookie vezane za token
+   
     //sliedi vertifikacija tokena
     //ako nema tokena
+
     if(!token)  return next(errorHandler('401','Unautorized'));
     //ako postoji token da provjrimo da li je ispravan ili ne,importujemo jwt 
-    jwt.verify(token,process.env.JWT_SECRET,(err,user)=>{
+
+    jwt.verify(token, process.env.JWT_SECRET , (err, user)=>{
         if(err) return next(errorHandler('403','Forbidden'))
+
 // u koliko je token ispravan izdvajamo korisnika i idemo na sledecu funkciju
-        req.user = user // sacuvali smo usera 
+
+        req.user = user; // sacuvali smo usera 
         next();
     })
     
 
     
-}
+};
