13064601bb3660c428578bc9dc392aa5a5275a23
Markost1 <markostevovic565@gmail.com>
2024-02-29 11:35:01 +0100

    kompletirano uploadovanje slika i funkcija brisanja slika

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,13 +1,69 @@
 import { useState } from "react"
-
-
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import {app} from '../firebase'
 
 
 export default function CreateListing() {
 
-  const [files,setFiles] = useState({})
+  const [files, setFiles] = useState({})
+  const [formData, setFormData] = useState({
+    imageUrls: []
+  })
+  const [imageUploadError , setImageUploadError] = useState(false)
 
-  console.log(files);
+  //console.log(files);
+  console.log(formData);
+  const handleImageSubmit = () =>{
+    //kreiranje uslova
+    if(files.length > 0 && files.length + formData.imageUrls.length < 7){
+      const promises = []
+      for (let i = 0; i < files.length; i++) {
+        promises.push(storeImage(files[i])) 
+        
+      }
+      Promise.all(promises).then((urls) => {
+        setFormData({...formData, imageUrls:formData.imageUrls.concat(urls),
+        });
+        setImageUploadError(false)
+      }).catch((err)=>{
+        setImageUploadError('Image upload failed (2mb Max per image)')
+      });
+    }else{
+      setImageUploadError('You can only upload 6 image per listing')
+    }
+}
+ const storeImage = (file) => {
+  return new Promise((resolve,reject)=>{
+    const storage = getStorage(app); //kreiranje skladista
+    const fileName = new Date().getTime() + file.name; // dajemo posebno ime svakom fajlu koje cuvamo u skladiste
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef,file);
+    uploadTask.on(
+      'state_changed',
+      (snapshot)=>{ // bez ovoga kod ne radi
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log(`Upload is ${progress}% done`);
+      },
+      (error) => {
+        reject(error)
+      },
+      ()=>{
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL)=>{
+          resolve(downloadURL);
+        });
+      }
+    )
+  })
+ };
+
+      const handleRemoveImage = (index) =>{
+        setFormData({
+          ...formData,
+          imageUrls: formData.imageUrls.filter((_, i) => //{ // u jesto url pisemo underline kad imam viticaste zagrade sve se brise bez njih radi ok
+            i !== index // }
+          ),
+        })
+ }
 
   return (
     <main className="p-3 max-w-4xl mx-auto">
@@ -91,10 +147,24 @@ export default function CreateListing() {
       {/* dio za uploadovanje slika */}
       <div className="flex gap-4">
         <input onChange={(e)=>{setFiles(e.target.files)}} className="p-3 border border-gray-300 rounded w-full" type="file" id="images" accept="image/*" multiple />
-        <button className="p-3 border border-green-700 rounded text-green-700 uppercase hover:shadow-lg disabled:opacity-85">Upload</button>
+        <button type="button" onClick={handleImageSubmit} className="p-3 border border-green-700 rounded text-green-700 uppercase hover:shadow-lg disabled:opacity-85">Upload</button>
+      
       </div>
+       <p className="text-red-700">{imageUploadError && imageUploadError}</p>
+      {
+        formData.imageUrls.length > 0 && formData.imageUrls.map((url,index)=>{
+          return (
+            <div key={url} className="flex justify-between p-3 border items-center">
+              <img src={url} alt='listing images' className="w-20 h-20 object-cover rounded-lg" />
+              <button type="button" onClick={()=>handleRemoveImage(index)} className=" p-3 text-red-500 uppercase hover:opacity-75">Delete</button>
+            </div>       
+          )
+  
+        // console.log(url) //vraca url ove
+        })
+      }
       <button className="bg-slate-600 p-3 rounded-lg text-yellow-50 uppercase hover:opacity-95 disabled:opacity-80">Create Listing</button>
-
+    
     </div>
       </form>
 
