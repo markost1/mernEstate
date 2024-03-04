129801c37a3523f9e3edbb6f168924b3b74e9bc6
Markost1 <markostevovic565@gmail.com>
2024-03-04 08:34:34 +0100

    User listing ruta kreirana , vracanje listinga od odgovarajuceg usera

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import bcrypt  from 'bcryptjs';
 import User from '../models/user.model.js';
 import { errorHandler } from "../utils/error.js";
+import Listing from '../models/listing.model.js';
 
 export const test = (req,res)=>{
     res.json({
@@ -78,4 +79,18 @@ export const deleteUser = async(req,res,next) =>{
         next(error);
     }
 
+}
+
+export const getUserListings = async (req,res,next) => {
+   // console.log(req.cookies);
+    if(req.user.id === req.params.id){
+try {
+    const listings = await Listing.find( { userRef: req.params.id } );
+    res.status(200).json(listings)
+} catch (error) {
+    return(error)
+}
+    }else{
+       return next(errorHandler('401','You can only view your own listing'))
+    }
 }
