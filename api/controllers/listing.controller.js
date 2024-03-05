a18e9e311b84ef00aaef72b9ddd759760b648ff2
Markost1 <markostevovic565@gmail.com>
2024-03-05 10:25:27 +0100

    Kreirana api rura ya delete listing i funkcija

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async(req,res,next) =>{
 try {
@@ -10,4 +11,24 @@ res.status(201).json(listing)
 } catch (error) {
     next(error)
 }
+}
+
+export const deleteListing = async(req,res,next) => {
+    const listing = await Listing.findById(req.params.id) // pronadji podatke iz listinga po id-u
+    if(!listing){  // ako podaci ne postoji vrati gredku
+    return next(errorHandler(404,'Listing not found'))
+    }
+//ako listing postoji provjeravamo da li je korisnik vlasnik listinga
+//provjeravamo da li je id korinsika koji salje zahtjev jednak userRef listinga 
+    if(req.user.id !== listing.userRef){
+        return next(401,'You can delete only your own listing')
+    }
+    try{
+        await Listing.findByIdAndDelete(req.params.id)
+        res.status(200).json('listing is succesffully deleted')
+    }catch(error){
+        next(error)
+    }
+
+
 }
